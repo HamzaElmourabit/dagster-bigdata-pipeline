3a40d861c524a3dcc1a7e7e7808ed383fd81abbe
HAMZA ELMOURABIT <hamzaelmourabit04@gmail.com>
2026-01-25 20:41:46 +0100

    Add files via upload

---
+++ dagster_etl_scheduled/assets/etl_assets.py
@@ -0,0 +1,23 @@
+﻿import pandas as pd
+from dagster import asset
+
+@asset
+def load_data():
+    df = pd.read_csv("input.csv")
+    print("Data loaded:\n", df.head())
+    return df
+
+@asset
+def transform_data(load_data):
+    df = load_data.copy()
+    df["total"] = df["quantity"] * df["price"]
+    print("Data transformed:\n", df.head())
+    return df
+
+@asset
+def save_data(transform_data):
+    df = transform_data
+    df.to_csv("output.csv", index=False)
+    print("Data saved to output.csv")
+    return "Success"
+
